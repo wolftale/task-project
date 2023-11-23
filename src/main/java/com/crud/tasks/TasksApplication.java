5b7e2f83bf6e0bf14eb0747d100aff4333c43db4
GP <gp@gpms.local>
2023-11-23 14:28:45 +0100

    Update

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,13 +1,15 @@
 package com.crud.tasks;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@OpenAPIDefinition
 @SpringBootApplication
 public class TasksApplication extends SpringBootServletInitializer {
-
+//public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
