502460603867c46d72ee31b84b7fbfc62b686e84
GP <gp@gpms.local>
2023-11-23 16:18:51 +0100

    Zadanie 23.3 dodanie maila do wiadomosci (CC)

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class Mail {
     private final String mailTo;
+    private final String toCc;
     private final String subject;
     private final String message;
 }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -8,6 +8,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -29,6 +31,7 @@ public class SimpleEmailService {
     private SimpleMailMessage createMailMessage(final Mail mail) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
+        Optional.ofNullable(mail.getToCc()).ifPresent(m -> mailMessage.setCc(mail.getToCc()));
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
         return mailMessage;
